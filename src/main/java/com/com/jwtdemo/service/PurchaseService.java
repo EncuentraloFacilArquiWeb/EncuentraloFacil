dfee48df3f1daeb1011d18f1717e59eb0de43143
SebastianMoreyra <u201925702@upc.edu.pe>
2023-09-27 23:27:53 -0500

    Purchase

---
+++ src/main/java/com/com/jwtdemo/service/PurchaseService.java
@@ -0,0 +1,23 @@
+package com.com.jwtdemo.service;
+import com.com.jwtdemo.controller.PurchaseController;
+import com.com.jwtdemo.model.Product;
+import com.com.jwtdemo.model.Purchase;
+import com.com.jwtdemo.model.Seller;
+import com.com.jwtdemo.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@Service
+public class PurchaseService {
+    private final PurchaseRepository purchaseRepository;
+    @Autowired
+    public  PurchaseService(PurchaseRepository purchaseRepository){this.purchaseRepository= purchaseRepository;}
+
+    public Purchase addPurchase(Purchase purchase){return purchaseRepository.save(purchase);}
+    public List<Purchase> lista() {
+        return purchaseRepository.findAll();
+    }
+}
