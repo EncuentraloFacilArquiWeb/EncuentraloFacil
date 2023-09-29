28fa53bc5a5eacdd67c46d758213ab40972434d1
TadashiBD <tadashidulantobuitron@gmail.com>
2023-09-29 15:31:55 -0500

    a

--- src/main/java/com/com/jwtdemo/controller/ProductController.java
+++ src/main/java/com/com/jwtdemo/controller/ProductController.java
@@ -31,10 +31,6 @@ public class ProductController {
         return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
     }
 
-    /*@GetMapping("/auth/api/product/list")
-    public ResponseEntity<List<Product>> list () {
-        return new ResponseEntity<>(productService.lista(), HttpStatus.OK);
-    }*/
 
     @DeleteMapping("/product/delete/{id}")
     public ResponseEntity<Product> elimina(@PathVariable Long id) throws Exception { return new ResponseEntity<>(productService .elimina(id), HttpStatus.OK); }
