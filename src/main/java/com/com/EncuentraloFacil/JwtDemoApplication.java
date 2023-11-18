26a579ad3b7daa8193326753b7e95d120f49acc0
TadashiBD <tadashidulantobuitron@gmail.com>
2023-11-17 23:14:39 -0500

    main

--- src/main/java/com/com/EncuentraloFacil/JwtDemoApplication.java
+++ src/main/java/com/com/EncuentraloFacil/JwtDemoApplication.java
@@ -12,13 +12,13 @@ public class JwtDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+	//@Bean
+	//public WebMvcConfigurer corsConfigurer() {
+	//	return new WebMvcConfigurer() {
+	//		@Override
+	//		public void addCorsMappings(CorsRegistry registry) {
+	//			registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+	//		}
+	//	};
+	//}
 }
